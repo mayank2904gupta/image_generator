776d49d30ca1def83e2fa9f70e8cfa6854164b96
mayank2904gupta <mayankgupta2904@gmail.com>
2025-03-03 10:14:59 +0530

    setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <span className="bg-gray-300  text-2xl">hello world</span>
+    </div>
+  );
+};
+
+export default App;
