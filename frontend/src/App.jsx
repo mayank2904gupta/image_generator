ad80db9d5b6e9b7392d92a1967f6bb2120b36a87
mayank2904gupta <mayankgupta2904@gmail.com>
2025-03-03 10:25:20 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import {Route,Routes} from "react-router-dom"
+import BuyCredits from "./pages/BuyCredits";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
 
 const App = () => {
   return (
     <div>
-      <span className="bg-gray-300  text-2xl">hello world</span>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/result" element={<Result/>} />
+        <Route path="/buy" element={<BuyCredits/>} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/BuyCredits.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BuyCredits = () => {
+  return (
+    <div>
+      <h1>buy credits please</h1>
+    </div>
+  );
+};
+
+export default BuyCredits;
